1cc11b6fd5aacca5876ff1da4fe8c3ff55e4fad9
Jonas Karlsson <jonaskarlsson@fripost.org>
2019-08-28 19:50:52 +0200

    Appimage: threaded make

--- appimage/make_appimage.sh
+++ appimage/make_appimage.sh
@@ -5,7 +5,6 @@ wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous
 chmod +x linuxdeploy-x86_64.AppImage
 cd build
 cmake ../.. -DDATA_DIR=/usr/share/puzzlemoppet/Puzzle/data -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
-make
-make install DESTDIR=AppDir
+make -j `nproc --all` install DESTDIR=AppDir
 cd ..
 ./linuxdeploy-x86_64.AppImage --appdir build/AppDir --output appimage -d puzzlemoppet.desktop -i ../icons/main.png
