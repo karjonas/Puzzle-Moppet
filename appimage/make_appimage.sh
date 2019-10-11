8ac65ac1433ebb7a74d0a5b9f9261871d24ec68d
Jonas Karlsson <jonaskarlsson@fripost.org>
2019-10-11 13:03:46 +0200

    Update appimage for vcpkg

--- appimage/make_appimage.sh
+++ appimage/make_appimage.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+rm -rf vcpkg
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg install irrlicht ode openal-soft
+cd ..
 rm -rf build
 mkdir build
-wget -nc https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+rm linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
 cd build
-cmake ../.. -DDATA_DIR=/usr/share/puzzlemoppet/Puzzle/data -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake ../.. -DCMAKE_TOOLCHAIN_FILE=$PWD/../vcpkg/scripts/buildsystems/vcpkg.cmake -DDATA_DIR=/usr/share/puzzlemoppet/Puzzle/data -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
 make -j `nproc --all` install DESTDIR=AppDir
 cd ..
 ./linuxdeploy-x86_64.AppImage --appdir build/AppDir --output appimage -d puzzlemoppet.desktop -i ../icons/main.png
